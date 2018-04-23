8004c2a362ecaf5a2e6f34628dbfacb4e17df7ce
Awilum <awilum@msn.com>
2018-04-23 14:53:26 +0300

    Navigation updates

--- site/themes/default/partials/navigation.php
+++ site/themes/default/partials/navigation.php
@@ -4,9 +4,14 @@
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'agent') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/agent">Agent</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'arr') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/arr">Arr</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'cookie') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/cookie">Cookie</a>
+    <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'curl') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/curl">Curl</a>
+    <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'date') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/date">Date</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'debug') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/debug">Debug</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'filesystem') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/filesystem">Filesystem</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'http') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/http">Http</a>
+    <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'number') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/number">Number</a>
+    <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'notification') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/notification">Notification</a>
+    <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'text') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/text">Text</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'token') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/token">Token</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'session') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/session">Session</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'registry') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/registry">Registry</a>
