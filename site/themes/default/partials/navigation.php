6e6cbac33399efce31462b2a16452ba78f0ec732
Awilum <awilum@msn.com>
2018-04-23 18:30:44 +0300

    navigation updates

--- site/themes/default/partials/navigation.php
+++ site/themes/default/partials/navigation.php
@@ -8,6 +8,7 @@
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'date') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/date">Date</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'debug') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/debug">Debug</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'filesystem') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/filesystem">Filesystem</a>
+    <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'html') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/html">Html</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'http') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/http">Http</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'number') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/number">Number</a>
     <a class="component-link <?php if (Flextype\Component\Http\Http::getUriSegment(0) == 'notification') echo 'active'; ?>" href="<?php echo Flextype\Component\Http\Http::getBaseUrl(); ?>/notification">Notification</a>
