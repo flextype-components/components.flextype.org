05285b27310843f86ae4be81495cf3dbea487ecc
Awilum <awilum@msn.com>
2018-05-21 20:32:14 +0300

    Update to Flextype 0.4.2

--- site/plugins/component/component.php
+++ site/plugins/component/component.php
@@ -4,7 +4,10 @@ use Flextype\Component\{Http\Http, Event\Event};
 
 Event::addListener('onPageContentAfter', function () {
     if (Http::getUriSegment(1) != '') {
-        Pages::$page['title'] = ucfirst(Http::getUriSegment(1)).' Component';
+
+        $page = Content::getCurrentPage();
+
+        Content::updateCurrentPage('title', ucfirst(Http::getUriSegment(1)).' Component');
 
         $component = Http::getUriSegment(1);
         $page_content = Markdown::parse(file_get_contents('https://raw.githubusercontent.com/flextype-components/'.$component.'/master/README.md'));
@@ -13,8 +16,8 @@ Event::addListener('onPageContentAfter', function () {
         $page_content .= '<a href="https://github.com/flextype-components/'.$component.'/releases">Download on the Github</a>';
         $page_content .= '<br><br><br>';
 
-        Pages::$page['content'] = $page_content;
-        Pages::$page['template'] = 'documentation';
+        Content::updateCurrentPage('content', $page_content);
+        Content::updateCurrentPage('template', 'documentation');
         Http::setResponseStatus(200);
     }
 });
